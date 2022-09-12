eb6f56e6a32a6e85611d1a4a3d9e6ae8dffc302d
EsCodeDiary <101074063+ESJoGithub@users.noreply.github.com>
2022-09-13 00:27:43 +0900

    tkinter MiniPhotoshop Project
    Canvas에 비율 맞춰 사진 올리기 완료
    +) 다른 canvas에 사진을 올리면 다른 객체에서 지워지는 문제 해결 필요.

--- tk_frame.py
+++ tk_frame.py
@@ -1,20 +1,23 @@
+from email.mime import image
 import tkinter as tk
 import tkinter.ttk as ttk
 from tk_mouse import MouseEvent
 from PIL import Image, ImageTk
+import cv2
 
 class Get_Frame(MouseEvent):
-  def __init__(self, window, count=1, width = 1000, height = 500, x = 30, y = 0):
+  def __init__(self, window, count=1, width = 1000, height = 510, x = 30, y = 0):
     super().__init__(window)
     self.window = window
     self.width = width
     self.height = height
+    self.photo_w = self.width
+    self.photo_h = self.height-10
     self.x = (count%5) * 30 + 30
     self.y = (count%10) * 50
     self.filename = "제목없음"
     self.name = str()
     self.count = count
-
   
   def get_filename(self, filename="제목없음"):
     if filename != "제목없음":
@@ -22,10 +25,10 @@ class Get_Frame(MouseEvent):
       self.filename = filename[r_index+1:]
     self.name = self.filename  + " - " + str(self.count)
   
-  def make_Frame(self, filename="제목없음", rgb_img=None):
+  def make_Frame(self, filename="제목없음"):
     self.get_filename(filename)
 
-    frame = tk.Frame(self.window, width=self.width, height=self.height, bd=1)
+    frame = tk.Frame(self.window, bd=1)
     frame.place(x=self.x, y=self.y)
     self.widget = frame
 
@@ -44,19 +47,39 @@ class Get_Frame(MouseEvent):
     canvas = tk.Canvas(frame2)
     canvas.pack(side="top")
 
-    if rgb_img is not None:
-      photo = Image.fromarray(rgb_img)
-      photo = ImageTk.PhotoImage(image=photo, master=canvas)
+    if filename != "제목없음":
+
+      src_origin = cv2.imread(filename)
+      h, w, c = src_origin.shape
+      if h > w :
+        self.width = 400
+        self.height = 710
+      if self.width/w < (self.height-10)/h:
+        self.photo_h = (int)((self.width/w) * h)
+        src = cv2.resize(src_origin, dsize = (self.width, self.photo_h))
+      elif self.width/w > (self.height-10)/h:
+        self.photo_w = (int)(((self.height-10)/h) * w)
+        src = cv2.resize(src_origin, dsize = (self.photo_w, self.height-10))
+
+      tk_img = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+      photo = Image.fromarray(tk_img)
+      paper = ImageTk.PhotoImage(image=photo, master=canvas)
       canvas.configure(width=self.width, height=self.height, relief="raised")
-      temp_width= self.width//2
-      temp_height= self.height//2
-      canvas.create_image(temp_width, temp_height, image=photo, anchor="nw")
-      
+      canvas.create_image(0, 0, image=paper, anchor="nw")
+
+      blob = photo.make_blob(format="RGB")
+      for i in range(0, self.photo_w):
+        for j in range(0, self.photo_h):
+          r = blob[i*3*self.photo_w + j*3 + 0]
+          g = blob[i*3*self.photo_w + j*3 + 1]          
+          b = blob[i*3*self.photo_w + j*3 + 2]
+          paper.put("#%02x%02x%02x" %(r, g, b), (j, i))  
+
     else:
       canvas.configure(width=self.width, height=self.height, relief="raised", bg="white")
 
     label2 = tk.Label(frame2, height=1)
-    label2.pack(side="bottom")
+    label2.pack(side="top")
 
     def drag(event):
       x = sizegrip.winfo_x() + event.x
@@ -74,7 +97,7 @@ class Get_Frame(MouseEvent):
                   y = canvas.winfo_y() + canvas.winfo_reqheight() + 30)
     sizegrip.bind("<ButtonRelease-1>", drag)
 
-    return frame, canvas
+    return frame, canvas, src_origin, tk_img
 
 
 
--- tk_methods.py
+++ tk_methods.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tk_frame import Get_Frame
 import cv2
+from PIL import Image, ImageTk
 import numpy as np
 
 class Methods(Get_Frame):
@@ -11,8 +12,6 @@ class Methods(Get_Frame):
     self.count_frames = 0
     self.framelist = []
     self.path = "C:\\Users\\user\\desktop"
-    self.width = 1000
-    self.height = 500
     self.origin = np.zeros(shape=(1000, 500, 3))
   
   def set_width(self, width = 1000):
@@ -32,7 +31,8 @@ class Methods(Get_Frame):
 
   def file_open(self):
     self.count_open()
-    sub_window = Get_Frame(self.window, self.count_frames, width=self.width, height=self.height)
+    sub_window = Get_Frame(self.window, self.count_frames)
+    
     _filename = filedialog.askopenfilename(initialdir=self.path, title="열기", 
                                           filetypes=(("모든 파일", "*.*"),
                                                     ("모든 그림 파일", "*.jpg;*.jpeg;*.btm;*.png;*.tif;*.gif"), 
@@ -41,13 +41,8 @@ class Methods(Get_Frame):
                                                     ("PNG", "*.png"),
                                                     ("TIF", "*.tif"),
                                                     ("GIF", "*.gif")))
-    src = cv2.imread(_filename)
-    src = cv2.resize(src, dsize = (self.width, self.height))
-    tk_img = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    self.origin = tk_img
-    sub_window.make_Frame(filename=_filename, rgb_img=tk_img)
-    
-    self.framelist.append(sub_window.widget)
+    frame, canvas, self.origin, tk_img = sub_window.make_Frame(filename=_filename)
+    self.framelist.append(frame)
 
   def close(self):
     self.window.quit()
